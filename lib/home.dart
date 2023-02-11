de36d901d020ed989fe617b0b26ab637f73bfaaa
leejunyoung <lolcy3205@dgist.ac.kr>
2023-02-11 21:06:03 +0900

    add weather, not color

--- lib/home.dart
+++ lib/home.dart
@@ -22,7 +22,7 @@ class _HomePageState extends State<HomePage> {
   /////////// temp//////??//////??//////??//////??//////??
   Future<dynamic> readJson() async {
     final String response =
-        await rootBundle.loadString("assets/etc/testcall.json");
+        await rootBundle.loadString("assets/etc/testcall2.json");
     final d = await json.decode(response);
     debugPrint("read jsonfile");
 
@@ -59,7 +59,13 @@ class _HomePageState extends State<HomePage> {
     var weather_path = data.dailyList[0].weather_path;
 
     return Container(
-      height: 130,
+      decoration: BoxDecoration(
+        border: Border.all(width: 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(5),
+      height: 170,
       child: Row(children: [
         Expanded(
           child: Column(
@@ -87,21 +93,22 @@ class _HomePageState extends State<HomePage> {
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Text(
-                    data.dailyList[0].av_temp,
+                    data.dailyList[0].av_temp + "°C",
                     style: TextStyle(fontSize: 18),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     width: 30,
                   ),
                   Text(data.dailyList[0].min_temp),
                   Text("~"),
                   Text(data.dailyList[0].max_temp),
+                  Text("(°C)")
                 ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Icon(
+                  const Icon(
                     Icons.water_drop_rounded,
                     color: Colors.blue,
                   ),
@@ -117,19 +124,100 @@ class _HomePageState extends State<HomePage> {
   }
 
   Widget timeline(var data) {
-    return Expanded(
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(width: 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      height: 130,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 100,
+        itemCount: data.hourlyList.length,
         itemBuilder: ((context, index) {
-          return Text("$index");
+          return Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              border: Border.all(width: 1),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            width: 110,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Image(
+                image: AssetImage(data.hourlyList[index].weather_path),
+                width: 50,
+                height: 50,
+              ),
+              Text(data.hourlyList[index].hour),
+              Text(data.hourlyList[index].av_temp +
+                  "°C"
+                      " / " +
+                  data.hourlyList[index].humidity +
+                  "%")
+            ]),
+          );
         }),
       ),
     );
   }
 
   Widget futureweather(var data) {
-    return Container();
+    return Container(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        border: Border.all(width: 1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(data.dailyList[1].day),
+            Image(
+              image: AssetImage(data.dailyList[1].weather_path),
+              width: 80,
+              height: 80,
+            ),
+            Text(
+              data.dailyList[1].av_temp,
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(data.dailyList[1].min_temp),
+            Text("~"),
+            Text(data.dailyList[1].max_temp),
+            Text("(°C)"),
+            Text(data.dailyList[1].humidity + "%"),
+          ],
+        ),
+        Divider(
+          thickness: 1,
+          color: Colors.grey,
+          indent: 10,
+          endIndent: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(data.dailyList[2].day),
+            Image(
+              image: AssetImage(data.dailyList[2].weather_path),
+              width: 80,
+              height: 80,
+            ),
+            Text(
+              data.dailyList[2].av_temp,
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(data.dailyList[2].min_temp),
+            Text("~"),
+            Text(data.dailyList[2].max_temp),
+            Text("(°C)"),
+            Text(data.dailyList[2].humidity + "%"),
+          ],
+        )
+      ]),
+    );
   }
 
   Widget restaurantGenerator() {
--- lib/weather.dart
+++ lib/weather.dart
@@ -22,7 +22,7 @@ class WholeWeather {
   }
 
   void getHourlyList(var whole) {
-    hourlyList = List<Weather>.generate(8, (index) {
+    hourlyList = List<Weather>.generate(16, (index) {
       return Weather(whole, id: index, isDaily: false);
     });
   }
@@ -40,6 +40,7 @@ class Weather {
   var weather, cloud, humidity;
   var weather_path;
   var rise_t, set_t, riseset;
+  var day, hour;
   // common
   var timeZone, timeOffset;
 
@@ -55,14 +56,19 @@ class Weather {
     timeOffset = whole["timezone_offset"];
 
     if (isDaily == true) {
-      av_temp = whole["daily"][id]["temp"]["day"].toString();
-      min_temp = whole["daily"][id]["temp"]["min"].toString();
-      max_temp = whole["daily"][id]["temp"]["max"].toString();
+      av_temp = whole["daily"][id]["temp"]["day"].round().toString();
+      min_temp = whole["daily"][id]["temp"]["min"].round().toString();
+      max_temp = whole["daily"][id]["temp"]["max"].round().toString();
 
       weather = whole["daily"][id]["weather"][0]["main"].toString();
       weather_path = "assets/images/$weather.png";
       cloud = whole["daily"][id]["clouds"].toString();
       humidity = whole["daily"][id]["humidity"].toString();
+
+      var t = whole["daily"][id]["dt"];
+      currentTime =
+          DateTime(1970, 01, 01).add(Duration(seconds: t + timeOffset));
+      day = DateFormat.Md().format(currentTime);
 
       rise_t = whole["daily"][id]["sunrise"];
       sunrise =
@@ -76,21 +82,16 @@ class Weather {
 
       riseset = rise_t + "/" + set_t;
     } else {
-      // hour for 0, 3, 6, 9, 12, 15, 18, 21
+      // hour for 16 time step per hour
 
-      if (id == 0) {
-        id = 0;
-      } else {
-        id = 3 * id - 1;
-      }
-
-      av_temp = whole["hourly"][id]["temp"];
-      humidity = whole["hourly"][id]["humidity"];
-      weather = whole["hourly"][id]["weather"][0]["main"];
+      av_temp = whole["hourly"][id]["temp"].round().toString();
+      humidity = whole["hourly"][id]["humidity"].toString();
+      weather = whole["hourly"][id]["weather"][0]["main"].toString();
       weather_path = "assets/images/$weather.png";
       var t = whole["hourly"][id]["dt"];
       currentTime =
           DateTime(1970, 01, 01).add(Duration(seconds: t + timeOffset));
+      hour = DateFormat.H().format(currentTime);
     }
   }
 }
